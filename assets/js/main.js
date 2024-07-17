317ad71f7cf41ec38dcccfa8c6cb9ed0e55cd389
itsMannu-Yadav <149852326+itsMannu-Yadav@users.noreply.github.com>
2024-07-17 22:02:28 +0530

    Addition of .vscode and asset (did this in VS Code bcz it was not working on website)

---
+++ assets/js/main.js
@@ -0,0 +1,64 @@
+/*==================== SHOW MENU ====================*/
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+
+/*==================== REMOVE MENU MOBILE ====================*/
+
+    // When we click on each nav__link, we remove the show-menu class
+
+
+/*==================== CHANGE BACKGROUND HEADER ====================*/
+
+    // When the scroll is greater than 100 viewport height, add the scroll-header class to the header tag
+
+/*==================== SWIPER DISCOVER ====================*/
+
+/*==================== VIDEO ====================*/
+
+        // Play video
+        // We change the icon
+       
+        // Pause video
+        // We change the icon
+
+
+    // Video ends, icon change
+    
+
+// ended, when the video ends
+
+
+/*==================== SHOW SCROLL UP ====================*/ 
+
+    // When the scroll is higher than 200 viewport height, add the show-scroll class to the a tag with the scroll-top class
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+
+/*==================== SCROLL REVEAL ANIMATION ====================*/
+
+
+
+/*==================== DARK LIGHT THEME ====================*/ 
+
+// Previously selected topic (if user selected)
+
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+
+
+// We validate if the user previously chose a topic
+
+  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+ 
+
+// Activate / deactivate the theme manually with the button
+
+    // Add or remove the dark / icon theme
+    
+    // We save the theme and the current icon that the user chose
+ 
